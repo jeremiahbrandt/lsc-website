35b6ba98f27e69a36339b45034850ce746a5855f
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2020-10-29 10:43:46 -0500

    Worked on events

--- pages/events.tsx
+++ pages/events.tsx
@@ -7,20 +7,23 @@ const eventsPage = () => {
     return (
         <Layout>
             <h1 className={"events-page-title"}>Upcoming Events</h1>
-            {
-                events.map((event, index) => {
-                   return (
-                       <article key={`event${index}`} className={"event"}>
-                           <h2>{event.name}</h2>
-                           <section>
-                               <span>When: {event.date}, {event.startTime} - {event.endTime}</span>
-                               <span>Location: {event.location}</span>
-                           </section>
-                           <p>{event.description}</p>
-                       </article>
-                   )
-                })
-            }
+            <div className={"events"}>
+                {
+                    events.map((event, index) => {
+                       return (
+                           <article key={`event${index}`} className={"event"}>
+                               <h2 className={"event-title"}>{event.name}</h2>
+                               <img src={event.image.src} alt={event.image.alt} />
+                               <section>
+                                   <span><b>When:</b> {event.date}, {event.startTime} - {event.endTime}</span>
+                                   <span><b>Location:</b> {event.location}</span>
+                               </section>
+                               <p>{event.description}</p>
+                           </article>
+                       )
+                    })
+                }
+            </div>
         </Layout>
     )
 }
