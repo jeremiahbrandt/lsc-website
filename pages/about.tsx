5541918dabb027e6932867d75e8ab92985541d24
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2020-10-30 16:40:42 -0500

    Add footer SVGs

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,15 @@
 import Layout from "../components/Layout"
 import Proxy from "../hooks/useProxy"
 import Bio from "../components/Bio";
+import Head from "next/head";
 
 const aboutPage = () => {
     const data = Proxy.getInformationPageData()
     return (
         <Layout>
+            <Head>
+                <title>LSC - About</title>
+            </Head>
             <div className={"bios"}>
                 <Bio {...data.lscBio} />
                 <Bio {...data.pastorBio} />
