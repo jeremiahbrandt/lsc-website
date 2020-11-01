eed216037764e33b86784314e25ff1c8b99f4e23
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2020-11-01 11:42:00 -0600

    Add Sanity.io CMS

---
+++ client.js
@@ -0,0 +1,7 @@
+import sanityClient from "@sanity/client"
+
+export default sanityClient({
+    projectId: "60gef8on",
+    dataset: "production",
+    useCdn: true
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import Slideshow from "../components/SlideShow";
 import Layout from "../components/Layout";
 import 'react-slideshow-image/dist/styles.css'
 import Events from "../components/Events";
+import client from "../client"
 
+export async function getStaticProps() {
+    const data = await client.fetch(`*[_type=="event"]`)
+    return {
+        props: {
+            data: data
+        }
+    }
+}
 
 export default function Home() {
     return (
