66d20bdc06fae4e0aa1f641e1e8688394367d924
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2020-11-05 14:16:34 -0600

    Mobile layout for Navigation and Footer

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,15 +1,30 @@
 import Link from 'next/link'
 import {getNavigationLinks} from "../lib/navigation"
 import styles from "../css/Navigation.module.css"
+import React, {useState} from "react";
 
 const Navigation = () => {
     const links = getNavigationLinks()
+
+    const [mobileLinksShown, setMobileLinksShown] = useState<boolean>(false);
+
+    const toggleMobileLinksShown = () => {
+        setMobileLinksShown(!mobileLinksShown)
+    }
+
     return (
-        <nav className={styles.nav}>
-            {
-                links.map((link, index) => <Link key={`navLink${index}`} href={link.url}><a className={styles.link}>{link.name}</a></Link>)
-            }
-        </nav>
+        <div className={styles.wrapper}>
+            <div className={styles.hamburger} onClick={toggleMobileLinksShown}>
+                <div className={styles.hamburgerLine} />
+                <div className={styles.hamburgerLine} />
+                <div className={styles.hamburgerLine} />
+            </div>
+            <nav className={`${styles.nav} ${mobileLinksShown ? styles.navShown : ""}`}>
+                {
+                    links.map((link, index) => <Link key={`navLink${index}`} href={link.url}><a className={styles.link}>{link.name}</a></Link>)
+                }
+            </nav>
+        </div>
     )
 }
 
