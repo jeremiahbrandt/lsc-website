85b26aaa4dc23b790e4b77389a253154051fecd2
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2021-01-16 15:47:44 -0600

    Re-added accidently removed props

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,9 +2,19 @@ import Layout from "../components/Layout"
 import ContactForm from "../components/ContactForm";
 import {IContactPageContent} from "../interfaces/cmsQueries/pages/IContactPageContent";
 import React from "react";
+import {getContactPageContent, getSiteConfig} from "../lib/api";
 import {IConfig} from "../interfaces/cmsQueries/pages/IConfig";
 import DaysOfOperation from "../components/DaysOfOperation";
 import styles from "../css/ContactPage.module.css"
+
+export async function getStaticProps() {
+    return {
+        props: {
+            content: await getContactPageContent(),
+            config: await getSiteConfig()
+        }
+    }
+}
 
 const ContactPage = (props: {content: IContactPageContent, config: IConfig}) => {
     return (
