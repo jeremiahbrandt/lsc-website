354c8ab842cfaac7cb8dca6c0bde37204d5c28f2
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2021-07-20 14:24:11 -0500

    Added coming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,11 +20,14 @@ export async function getStaticProps() {
 
 const IndexPage = (props: {content: IHomePageContent, config: IConfig}) => {
     return (
-        <Layout title={"Home"} config={props.config}>
-            <Video name={props.content.welcomeVideo.video.name} id={props.content.welcomeVideo.video.id} />
-            <Slideshow imageUrls={props.content.slideshowImages} />
-            <Events events={props.content.events} sectionTitle={props.content.eventsSectionTitle} />
-        </Layout>
+        // <Layout title={"Home"} config={props.config}>
+        //     <Video name={props.content.welcomeVideo.video.name} id={props.content.welcomeVideo.video.id} />
+        //     <Slideshow imageUrls={props.content.slideshowImages} />
+        //     <Events events={props.content.events} sectionTitle={props.content.eventsSectionTitle} />
+        // </Layout>
+        <div style={{display: 'grid', justifyContent: 'center', alignContent: 'center', backgroundColor: '#000000', color: '#e2e2e3', height: '100vh'}}>
+            <h1 style={{fontSize: 'xxx-large'}}>Coming Soon</h1>
+        </div>
     )
 }
 
