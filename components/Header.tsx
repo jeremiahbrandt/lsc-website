b1c932118b63d4a0047396d1e43324095c040136
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2021-10-07 13:03:26 -0500

    Added site title to Main Naviation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,23 @@
 import Navigation from "./Navigation";
 import Link from "next/link";
-import React, {FC} from "react"
+import React, { FC } from "react"
 import styles from "../css/Header.module.css"
 
-const Header:FC<{logo: string}> = ({logo}) => {
+export type HeaderProps = {
+    logo: string
+    title: string
+}
+
+const Header: FC<HeaderProps> = ({ logo, title }: HeaderProps) => {
     return (
         <div className={styles.wrapper}>
             <header className={styles.header}>
-                <Link href={"/"}><img className={styles.img} src={logo} alt={"logo"} /></Link>
+                <Link href={"/"}>
+                    <div className={styles.logo}>
+                        <img className={styles.img} src={logo} alt={"logo"} />
+                        <div className={styles.title}>{title}</div>
+                    </div>
+                </Link>
                 <Navigation />
             </header>
         </div>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ import styles from "../css/Layout.module.css"
 const Layout:React.FC<{title: string, config: IConfig}> = ({children, title,config}) => {
     return (
         <div className={"layout"}>
-            <Header logo={config.logo} />
+            <Header logo={config.logo} title={config.siteTitle} />
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>Lutheran Student Center | {title}</title>
